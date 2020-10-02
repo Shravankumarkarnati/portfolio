9c84fce5d92c59ba3f5fc14ba3f6629c83609451
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2020-10-02 13:12:48 -0400

    Updates

---
+++ precache-manifest.f5c3a7ea5b6bc3c32c6624cc7137dc54.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b609d7f48834293b7d950d5197612a57",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "19fdc7d1ac2e3069f0d4",
+    "url": "/portfolio/static/js/2.0f697da3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0f697da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ff140d2b3ff92b6ee8",
+    "url": "/portfolio/static/js/main.914b606f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "cbf13ad9eb30a233de27fe84d9a45adc",
+    "url": "/portfolio/static/media/A-Star.cbf13ad9.png"
+  },
+  {
+    "revision": "575b74f0dad62c3fcdccc8f69fc36ebf",
+    "url": "/portfolio/static/media/Shravan Kumar Karnati.575b74f0.pdf"
+  },
+  {
+    "revision": "527a37ac0e6d78d132ce58dd4a04b37d",
+    "url": "/portfolio/static/media/corterie.527a37ac.png"
+  },
+  {
+    "revision": "7a3fa40d7f1369502ec95c27ff9859b4",
+    "url": "/portfolio/static/media/moon.7a3fa40d.svg"
+  },
+  {
+    "revision": "87fd38eadcb57a0355d5d89beacfa6eb",
+    "url": "/portfolio/static/media/regale.87fd38ea.png"
+  },
+  {
+    "revision": "923e33daec1eb28496f1546c20ca0f1d",
+    "url": "/portfolio/static/media/shorts.923e33da.png"
+  },
+  {
+    "revision": "b6f486177d75b749d993c0d123f514b7",
+    "url": "/portfolio/static/media/sun.b6f48617.svg"
+  },
+  {
+    "revision": "d8b755b07c72cfcf606fe6634b2091cc",
+    "url": "/portfolio/static/media/travelLog.d8b755b0.png"
+  }
+]);
